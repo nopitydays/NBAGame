5f2f6ad1ba6c3ea492e066f543579800a334f637
nopitydays <nopitydays@gmail.com>
2018-12-26 21:52:43 +0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 var WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
-var ropstenPrivateKey = new Buffer("dfd30655069bbbecc00b41dcc944473c6d750cf155146219db7dc06052d558d7","hex");
+var ropstenPrivateKey = new Buffer("YOUR_PRIVATE_KEY_HERE","hex");
 var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
-var ropstenProvider = new WalletProvider(ropstenWallet, "https://ropsten.infura.io/v3/9c8ce7c8d48a406aaec814e627c233dd");
+var ropstenProvider = new WalletProvider(ropstenWallet, "YOUR_ENDPOINT_IN_INFURA");
 module.exports = {
 
   networks: {
